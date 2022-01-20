df0e52a06d82235f580970798a5f150609671f18
Katherine Fu <katherine@MacBook-Pro.local>
2022-01-20 20:44:11 +0100

    commit

--- day7/ex02/inc/Array.hpp
+++ day7/ex02/inc/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: kfu <kfu@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/19 16:05:18 by kfu           #+#    #+#                 */
-/*   Updated: 2022/01/19 17:57:54 by kfu           ########   odam.nl         */
+/*   Updated: 2022/01/20 20:40:24 by katherine     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,22 +26,28 @@ class Array
 	public:
 		Array() : _MyArray(new T[0]), _Size(0) {}; 
 		Array(unsigned int N) : _MyArray(new T[N]), _Size(N) {};
+
 		Array(const Array &Copy) {
-			if (_Size != 0)
+			_MyArray = new T[Copy._Size];
+			for (int i = 0; i < _Size; ++i)
+				_MyArray[i] = Copy._MyArray[i];
+		};
+
+		Array &operator= (const Array &Copy) {
+			if (this != &Copy)
 			{
+				this->~Array();
+				_Size = Copy._Size;
 				_MyArray = new T[_Size];
 				for (int i = 0; i < _Size; ++i)
 					_MyArray[i] = Copy._MyArray[i];
 			}
-			else
-				_MyArray = new T[0];
 			return (*this);
 		};
-		Array &operator= (const Array &Copy) {
-			for (int i = 0; i < _Size; ++i)
-					_MyArray[i] = Copy._MyArray[i];
+
+		~Array() {
+			delete[] _MyArray;
 		};
-		~Array() {};
 
 		int		Size(void) const {
 			return (_Size);
@@ -55,6 +61,13 @@ class Array
 					return ("Element is out of limits");
 				};
 		};
+
+		T &operator[] (int Index) const
+		{
+			if (Index < 0 || Index >= _Size)
+				throw ElementOutOfLimits();
+			return (_MyArray[Index]);
+		}
 };
 
 #endif
--- day7/ex02/main.cpp
+++ day7/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kfu <kfu@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/19 15:25:22 by kfu           #+#    #+#                 */
-/*   Updated: 2022/01/19 17:58:23 by kfu           ########   odam.nl         */
+/*   Updated: 2022/01/20 20:42:28 by katherine     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,26 @@
 
 int	main(void)
 {
-	Array <int>MyArray(5);
+	Array<std::string> CharArr(5);
+
+	for (int i = 0; i < 5; i++)
+	{
+		CharArr[i] = std::string("Arr: " + std::to_string(i));
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << CharArr[i] << std::endl;
+	}
+
+	std::cout << "---------------------------------" << std::endl;
+	Array<std::string> CharArr2;
+
+	CharArr2 = CharArr;
+
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << CharArr2[i] << std::endl;
+	}
 	return (0);
 }
