5028cf2213300042f2134f7e07e97a9bdf5bedf1
Katherine Fu <katherine@MacBook-Pro.local>
2022-01-21 14:05:09 +0100

    commit

--- day8/ex00/inc/EasyFind.hpp
+++ day8/ex00/inc/EasyFind.hpp
@@ -6,7 +6,7 @@
 /*   By: katherine <katherine@student.codam.nl>       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/21 09:18:38 by katherine     #+#    #+#                 */
-/*   Updated: 2022/01/21 09:40:29 by katherine     ########   odam.nl         */
+/*   Updated: 2022/01/21 14:02:49 by katherine     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,27 @@
 # define EASYFIND_HPP
 
 # include <iostream>
+# include <exception>
+# include <iterator>
 
-template <typename T> int EasyFind(T Container, int Value);
+class CantFindValue : public std::exception
+{
+	public:
+		const char *what() const throw()
+		{
+			return ("Can't find the value");
+		};
+};
+
+template <typename T>
+typename T::iterator EasyFind(T &Container, int Value)
+{
+	for (typename T::iterator It = Container.begin(); It != Container.end(); It++)
+	{
+		if (*It == Value)
+			return (It);
+	}
+	throw CantFindValue();
+}
 
 #endif
--- day8/ex00/main.cpp
+++ day8/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: katherine <katherine@student.codam.nl>       +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/21 09:19:05 by katherine     #+#    #+#                 */
-/*   Updated: 2022/01/21 09:48:38 by katherine     ########   odam.nl         */
+/*   Updated: 2022/01/21 14:02:15 by katherine     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,18 @@
 
 int		main(void)
 {
-	std::cout << "hello" << std::endl;
-	return (0);
+	int	MyArray[10];
+
+	for (int i = 0; i < 10; i++)
+		MyArray[i] = i;
+
+	try
+	{
+		std::cout << EasyFind(MyArray, 2) << std::endl;
+	}
+	catch(const std::exception& Exc)
+	{
+		std::cerr << Exc.what() << std::endl;
+	}
+	
 }
